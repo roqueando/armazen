3f22c7d4f20bdd8851b2cd49cc4f7b3a71e42aa0
Vitor Roque <vitor.roquep@gmail.com>
2024-06-30 23:20:41 -0300

    feat: initial crab commit

--- include/armazen/utils/tokens.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-
-namespace utils
-{
-  std::vector<std::string> remount_without_symbols(std::vector<std::string> tokens);
-  bool is_unique_symbol(std::string token);
-}
--- src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main(int argc, char *argv[])
-{
-  std::cout << "Hello there";
-  return 0;
-}
