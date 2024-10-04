9e8f9a35823c63c5d12b6cacad6845b5f1609f1f
Vitor Roque <vitor.roquep@gmail.com>
2024-10-04 11:01:31 -0300

    fix: something

--- src/Data/Log/Types.hs
+++ src/Data/Log/Types.hs
@@ -1,9 +1,39 @@
 module Data.Log.Types (Layer (..)) where
 
-data Layer = Length Int | Token String
+data Layer =  Length Int | Token String
   deriving (Show, Eq)
+  -- fn :: Num a => a
 
-data Param = NonNumerical | Wildcard String
+  {--
+     Length 5
+        |
+        |
+  Token "Temperature"
+        |
+        |
+  Token "*"
+        |
+        |
+  Token "exceeds"
+        |
+        |
+    LogGroup [Parameter]
+    --> Parameter { event = "Temperature * exceeds", params = [Param { position = 0, value = "(43C)" }] }
+    --}
+  {--
+     Length 5
+        |
+        |
+  TokenData { token = "Temperature", param = NotParam }
+        |
+        |
+  TokenData { token = "*", param = Param "(43C)" }
+        |
+        |
+  TokenData { token = "exceeds", param = NotParam}
+    --}
+
+data Param = NotParam | Param String
   deriving (Show, Eq)
 
 data TokenData = TokenData
